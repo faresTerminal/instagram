8472b7a162a8c1a07ff356aa2e8b7aed1d1985c6
faresTerminal <38914620+faresTerminal@users.noreply.github.com>
2022-12-02 13:13:43 +0100

    update setting

--- instagram/settings.py
+++ instagram/settings.py
@@ -120,9 +120,11 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
+#STATIC_URL = '/static/'
+#MEDIA_URL = '/media/'
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
